5dce09c24a0429ba096d6814956b1fe951fe31d7
Manoj Khati <mksea18@gmail.com>
2024-09-26 14:20:45 +0545

    changed

--- src/components/homeComponent/jobCard.jsx
+++ src/components/homeComponent/jobCard.jsx
@@ -3,12 +3,16 @@ import {Link } from 'react-router-dom';
 
 const JobCard=({job})=> {
   return (
-    <div className='flex flex-col justify-evenly shadow-md rounded-md h-64 w-64 bg-red-100 mt-10transition duration-150 ease-in-out '>
+    <div className='flex flex-col items-center justify-evenly ml-52 mr-52 bg-red-100 mt-10 h-96 w-96 transition duration-150 ease-in-out transform hover:scale-105 hover:shadow-lg rounded-lg '>
         <div className=''>
-      <h3>{job.title}</h3>
-      <p>{job.company}</p>
-      <p>{job.location}</p>
-      <Link to={`/jobs/${job}`}>View details</Link>
+            <div>
+            <h3 className='text-xl font-bold text-gray-800'>{job.title}</h3>
+            </div>
+      <div>
+        <p className='text-gray-600 mt-2'>{job.company}</p>
+      </div>
+      <p className='text-gray-500 mt-4'>{job.location}</p>
+      <Link to={`/jobs/${job}` } className='text-blue-500 font-semibold hover:text-red-700 transition-colors duration-300'>View details</Link>
     </div>
     </div>
   )
--- src/components/shareComponent/header.jsx
+++ src/components/shareComponent/header.jsx
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 
 const  Header=()=>{
   return (
-    <div className='container bg-gradient-to-r from-black to-indigo-400  mt-4  sticky top-0  shadow-lg rounded-md'>
+    <div className='   bg-gradient-to-r from-black to-gray-400  mt-4  sticky top-0  shadow-lg p-1'>
+
         <div className='logo-container '> 
         <Link to="/">
-            <img src="/logo.png" alt="logo" className="logo w-16 h-16 rounded-md text-white flex items-center justify-center" />
+            <img src="/logo.png" alt="logo" className="logo w-16 h-16 rounded-md flex items-center justify-center" />
           </Link>     
           </div>     
 
-                    <ul className='flex items-center justify-around'>
-                    <li className='flex items-cente text-white'><Link to ="/">Home</Link></li>
-                    <li className='text-white'><Link to="/jobs">Jobs</Link></li>
-                    <li className='text-white flex justify-end'><Link to="/employer-dashboard">Employer Dashboard</Link></li>
-                    <li className='text-white flex justify-end'> <Link to="/candidate-dashboard">Candidate Dashboard</Link></li>
+                    <ul className=' flex items-center justify-center space-x-12 '>
+                    <li className=' text-white font-medium hover:text-gray-900 hover:bg-white'><Link to ="/">Home</Link></li>
+                    <li className='text-white font-medium hover:text-gray-900'><Link to="/jobs">Jobs</Link></li>
+                    <li className='text-white font-medium hover:text-gray-900'><Link to="/employer-dashboard">Employer Dashboard</Link></li>
+                    <li className='text-white font-medium hover:text-gray-900'> <Link to="/candidate-dashboard">Candidate Dashboard</Link></li>
 
                 </ul>
            
